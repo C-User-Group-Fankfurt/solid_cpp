6f55884d347b7b932e0029d01642d23ae2c91089
Daniel Dube <danieldube@users.noreply.github.com>
2023-09-06 17:31:01 +0200

    Remove the Actor.

--- src/driving_system.cpp
+++ src/driving_system.cpp
@@ -9,19 +9,9 @@ class Sensor {
   }
 };
 
-struct SteeringAngle {};
-struct Acceleration {};
-struct Torque {};
-
-class Actor {
-  void set_steering_angle(const SteeringAngle & /*steering_angle*/) {}
-  void set_power_train(const Acceleration & /*acceleration*/) {}
-  void set_brake_torque(const Torque & /*brake_torque*/) {}
-};
-
 class Trajectory {
  public:
-  void drive(Actor & /*actor*/) {}
+  void drive() {}
 };
 
 class Planner {
@@ -29,37 +19,31 @@ class Planner {
   Trajectory plan_vehicle_behavior(const EnvironmentModel &/*environment*/) {
     return Trajectory{};
   }
-
-  void drive(Actor & /*actor*/) {}
 };
 
 class DrivingSystem {
  public:
   DrivingSystem(std::shared_ptr<Sensor> sensor,
-                std::shared_ptr<Planner> planner,
-                std::shared_ptr<Actor> actor) :
+                std::shared_ptr<Planner> planner) :
       sensor(std::move(sensor)),
-      planner(std::move(planner)),
-      actor(std::move(actor)) {}
+      planner(std::move(planner)) {};
 
   void one_cycle() {
     auto environment_model = sensor->model_environment();
     auto vehicle_trajectory = planner->plan_vehicle_behavior(environment_model);
-    vehicle_trajectory.drive(*actor);
+    vehicle_trajectory.drive();
   }
 
  private:
   std::shared_ptr<Sensor> sensor;
   std::shared_ptr<Planner> planner;
-  std::shared_ptr<Actor> actor;
 };
 
 int main(int, char **) {
   auto sensor = std::make_shared<Sensor>();
   auto planner = std::make_shared<Planner>();
-  auto actor = std::make_shared<Actor>();
 
   DrivingSystem driving_system(
-      sensor, planner, actor);
+      sensor, planner);
   driving_system.one_cycle();
 }
