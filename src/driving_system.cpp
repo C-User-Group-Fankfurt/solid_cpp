eaad6a33c46847e9694d55eeb6ff768f40b81dc0
Maximilian Rieder <mxmlnrdr@users.noreply.github.com>
2023-09-25 17:58:30 +0200

    add deceleration limiter class

--- src/driving_system.cpp
+++ src/driving_system.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
+#include <utility>
 #include <vector>
+#include <functional>
 
 struct EnvironmentModel {};
 class Sensor {
@@ -54,33 +56,19 @@ class PowerTrain final : public Actor {
   Acceleration acceleration_limit;
 };
 
-class Brake final : public DrivingModeAwareActor {
+using DecelerationLimitCallback = std::function<Acceleration()>;
+class Brake final : public Actor {
  public:
-  explicit Brake(const Acceleration &deceleration_limit) : deceleration_limit(
-      deceleration_limit) {};
+  explicit Brake(DecelerationLimitCallback get_deceleration_limit) : get_current_deceleration_limit(
+          std::move(get_deceleration_limit)) {};
 
   void control_vehicle(const Trajectory &) override {
-    auto &current_deceleration_limit = get_current_deceleration_limit();
+    auto current_deceleration_limit = get_current_deceleration_limit();
     std::cout << current_deceleration_limit << std::endl;
   };
 
-  void set_driving_mode(const DrivingMode &driving_mode) override {
-    current_driving_mode = driving_mode;
-  }
-
  private:
-  [[nodiscard]] const Acceleration &
-  get_current_deceleration_limit() const {
-    static const Acceleration
-        unlimited_deceleration{std::numeric_limits<double>::lowest()};
-    if (current_driving_mode == DrivingMode::normal)
-      return deceleration_limit;
-    else
-      return unlimited_deceleration;
-  }
-
-  Acceleration deceleration_limit;
-  DrivingMode current_driving_mode{DrivingMode::normal};
+    DecelerationLimitCallback get_current_deceleration_limit;
 };
 
 using NewtonMetre = double;
@@ -122,23 +110,51 @@ class DrivingSystem {
   Actors actors;
 };
 
+class DecelerationLimitHandler {
+public:
+    explicit DecelerationLimitHandler(const Acceleration &deceleration_limit)
+            : deceleration_limit(deceleration_limit){};
+
+    void set_driving_mode(const DrivingMode &driving_mode) {
+        current_driving_mode = driving_mode;
+    }
+
+    Acceleration get_current_deceleration_limit() {
+        static const Acceleration
+                unlimited_deceleration{std::numeric_limits<double>::lowest()};
+        if (current_driving_mode == DrivingMode::normal)
+            return deceleration_limit;
+        else
+            return unlimited_deceleration;
+    }
+
+private:
+    Acceleration deceleration_limit;
+    DrivingMode current_driving_mode{DrivingMode::normal};
+};
+
 int main(int, char **) {
   auto sensor = std::make_shared<Sensor>();
   auto planner = std::make_shared<Planner>();
 
   auto power_train = std::make_shared<PowerTrain>(
       Acceleration{MetresPerSquareSecond(13.6)});
-  auto brake = std::make_shared<Brake>(
-      Acceleration{MetresPerSquareSecond{21.0}});
+  auto deceleration_limiter = std::make_shared<DecelerationLimitHandler>(Acceleration{MetresPerSquareSecond{21.0}});
+  auto brake = std::make_shared<Brake>([&deceleration_limiter](){return deceleration_limiter->get_current_deceleration_limit();});
   auto steering_wheel = std::make_shared<SteeringWheel>(
       Torque{NewtonMetre{3.0}});
 
   DrivingSystem driving_system(sensor, planner,
                                {power_train, brake, steering_wheel});
 
-  const auto driving_mode = DrivingMode::normal;
-  brake->set_driving_mode(driving_mode);
+  auto driving_mode = DrivingMode::normal;
+  deceleration_limiter->set_driving_mode(driving_mode);
   steering_wheel->set_driving_mode(driving_mode);
+
+  driving_system.one_cycle();
+
+  driving_mode = DrivingMode::emergency;
+  deceleration_limiter->set_driving_mode(driving_mode);
 
   driving_system.one_cycle();
 }
